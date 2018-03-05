e8aabc3c4732df0effafd784c9a121e9f6da45ff
Erika Bueno <erika.bueno@radixeng.com.br>
2018-03-05 18:34:40 -0300

    first commit

---
+++ ModalView/TestNib.swift
@@ -0,0 +1,21 @@
+//
+//  TestNib.swift
+//  ModalView
+//
+//  Created by Erika Bueno on 05/03/18.
+//  Copyright © 2018 Erika Bueno. All rights reserved.
+//
+
+import UIKit
+
+class TestNib: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
